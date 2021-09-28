a76890c47ea1d90b0bde468786dbc8374c4c37ad
kiescha <90151742+kiescha@users.noreply.github.com>
2021-09-28 12:18:56 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let add = 2+8;
+document.write(add);
+
+let size=12;
+let double=size*2;
+document.write(double);
