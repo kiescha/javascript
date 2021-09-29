b8b7aa2149802aa856dce5e5f2bacfdcb4c967ca
kiescha <90151742+kiescha@users.noreply.github.com>
2021-09-29 22:46:59 +0200

    hamburger2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-let tasty = 'hamburger';
+alert('Hello')
+let tasty = 'hamburger ';
 document.write (tasty);
-let ingridient1 = 'meat';
-let ingridient2 = 'cheese';
-let ingridient3 = 'tomato';
-let ingridient4 = 'bread'
+let ingridient1 = 'meat ';
+let ingridient2 = 'cheese ';
+let ingridient3 = 'tomato ';
+let ingridient4 = 'bread '
 let allingridients = ingridient1 + ingridient2 + ingridient3 + ingridient4;
 document.write (allingridients);
