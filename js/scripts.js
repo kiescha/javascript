3259fc52a148b35d7db5c0008e5cbca06d06241d
kiescha <90151742+kiescha@users.noreply.github.com>
2021-10-02 21:28:07 +0200

    Loops and conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,9 @@
-alert('Watch out! Pokedex Incoming!')
-let pokemonList=[
-  {name:'Balbasaur', height: 0.7, type:['grass','poison']},
-  {name:'Metapod', height: 0.7, type:'bug'},
-  {name:'Beedrill', height:1, type:['bug', 'poison']}
-];
-/* Wich is better? i dont know yet!
+//alert('Watch out! Pokedex Incoming!')
 let pokemonList=[
   {
     name:'Balbasaur',
     height: 0.7,
-    type:[
-      'grass',
-      'poison'
-    ]
+    type:['grass','poison']
   },
   {
     name:'Metapod',
@@ -22,9 +13,13 @@ let pokemonList=[
   {
     name:'Beedrill',
     height:1,
-    type:[
-      'bug',
-      'poison'
-    ]
+    type:['bug', 'poison']
   }
-];*/
+];
+for ( let i=0; i < pokemonList.length; i++){
+  if (pokemonList[i].height < 0.9){
+    document.write(pokemonList[i].name +' (Height:'+ pokemonList[i].height+')');
+  } else {
+    document.write(pokemonList[i].name +' (Height:'+ pokemonList[i].height+')' + ' Wow that\'s big!');
+  }
+}
